04077b7557876103073bf3bce60018badeac2473
Henrik Uggla <henrik.uggla@kristianstad.se>
2018-05-21 11:47:26 +0200

    Delete run_test.sh

--- backup/run_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker run --rm -ti -e 'PYTHONPATH=/code/src' -v $PWD:/code webcenter/nosetests-python2.7:latest bash -c "pip install --upgrade -r /code/requirements.txt && nosetests -w test/backup"
