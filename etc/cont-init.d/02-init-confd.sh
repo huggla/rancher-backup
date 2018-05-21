8325cf621acb6e71a4deda61c298b7327ce4c595
Henrik Uggla <henrik.uggla@kristianstad.se>
2018-05-21 11:33:53 +0200

    Delete 02-init-confd.sh

--- etc/cont-init.d/02-init-confd.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/with-contenv bash
-
-if [ "${CONFD_NODES}X" == "X" ]; then
-  NODE=""
-else
-  NODE="-node ${CONFD_NODES}"
-fi
-
-${CONFD_HOME}/bin/confd -confdir ${CONFD_HOME}/etc -onetime -backend ${CONFD_BACKEND} ${PREFIX} ${NODE}
